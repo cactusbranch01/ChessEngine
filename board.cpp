e2490f058f336ccff0cff7a0de7142fdb2a5aa0b
cactusbranch01 <83898055+cactusbranch01@users.noreply.github.com>
2023-01-25 11:59:42 -0800

    add board class

---
+++ board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by benrr on 1/25/2023.
+//
+
+#include "board.h"
---
+++ board.h
@@ -0,0 +1,14 @@
+//
+// Created by benrr on 1/25/2023.
+//
+
+#ifndef MATRIXCHESSENGINECODE_BOARD_H
+#define MATRIXCHESSENGINECODE_BOARD_H
+
+
+class board {
+
+};
+
+
+#endif //MATRIXCHESSENGINECODE_BOARD_H
