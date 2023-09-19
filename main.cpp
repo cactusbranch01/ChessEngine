a8b84cfea675caa405ecd4526f7b056c8438241a
cactusbranch01 <benradroberts@gmail.com>
2023-09-18 21:23:19 -0700

    changing depth req

--- main.cpp
+++ main.cpp
@@ -404,13 +404,13 @@ std::vector<std::vector<std::vector<int>>> ply(const std::vector<std::vector<int
 
 int main() {
     std::vector<std::vector<int>> chessboard = initializeChessboard();
-    
-    int depth = 3;
+
+    int depth = 2;
 
     // Generate all possible positions up to the specified depth
     std::vector<std::vector<std::vector<int>>> allPossiblePositions = ply(chessboard, depth, 1);
 
-    displayChessboard(allPossiblePositions[9999]);
+    displayChessboard(allPossiblePositions[197741]);
     std::cout << allPossiblePositions.size() << std::endl;
 
     return 0;
